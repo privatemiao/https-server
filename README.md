201205a8e3b51dc90689b1aa1740db26338c95ff
Mel Liu <privatemiao@gmail.com>
2017-12-04 11:56:43 +0800

    lollipop

---
+++ README.md
@@ -0,0 +1,2 @@
+#HTTPS
+keytool -genkey -alias ssl -keyalg RSA -keypass password -storepass password -keystore ssl-server.jks
---
+++ src/main/java/org/mel/httpsserver/HttpsServerApplication.java
@@ -0,0 +1,12 @@
+package org.mel.httpsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsServerApplication.class, args);
+	}
+}
